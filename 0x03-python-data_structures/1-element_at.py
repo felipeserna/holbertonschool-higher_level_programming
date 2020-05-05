33466de352443b4c5c242eb9e1e69126c42ab928
Felipe Serna <feserna86@gmail.com>
2020-05-05 20:39:03 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,7 @@ def element_at(my_list, idx):
         return (None)
     if idx > len(my_list):
         return (None)
-    for i in range(len(my_list)):
-        if my_list[i] == my_list[idx]:
-            return (my_list[i])
+    else:
+        for i in range(len(my_list)):
+            if my_list[i] == my_list[idx]:
+                return (my_list[i])
