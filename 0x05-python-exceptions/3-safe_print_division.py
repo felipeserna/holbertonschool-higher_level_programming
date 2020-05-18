38568b37fb7d96ad1894c3ed9ed893bd03b3827d
Felipe Serna <feserna86@gmail.com>
2020-05-18 22:31:40 +0000

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    division = None
+    try:
+        division = a/b
+    except ValueError:
+        return(None)
+    except ZeroDivisionError:
+        return(None)
+    finally:
+        print("Inside result: {}".format(division))
+        return(division)
