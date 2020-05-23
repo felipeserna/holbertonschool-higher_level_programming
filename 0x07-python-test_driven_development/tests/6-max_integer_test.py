39ff6fe3d8f7e2ffb270d9c6f9b78a479d4b2dfa
Felipe Serna <feserna86@gmail.com>
2020-05-23 03:48:19 +0000

    task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    tests the maximum integer
+    """
+    def test_with_integers(self):
+        """tests with integer values"""
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([-5, -30, -15, -23]), -5)
+        self.assertEqual(max_integer([3.4, -2.1]), 3.4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([float('inf'), 3]), float('inf'))
+        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+        self.assertEqual(max_integer([1.3, 23, 10, 90.1, 90.2]), 90.2)
+
+    def test_with_strings(self):
+        """tests with strings values"""
+        self.assertEqual(max_integer("holberton"), 't')
+        self.assertEqual(max_integer("ABabcCzZ234"), 'z')
+        self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
+
+    def test_errors(self):
+        """raises errors from max_integer"""
+        self.assertRaises(TypeError, max_integer, [5j, 3j])
+        self.assertRaises(TypeError, max_integer, 25)
