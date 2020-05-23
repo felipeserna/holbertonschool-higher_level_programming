014bfb8fd8c3c7b2e832af19e17e6b38c8e74bae
Felipe Serna <feserna86@gmail.com>
2020-05-23 02:04:51 +0000

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+prints a text with 2 new lines
+after each of these
+characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Args: text: string.
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    characters = [':','.','?']
+    start_of_line = True
+
+    for i in text:
+        if i == ' ' and start_of_line is True:
+            continue
+        if i in characters:
+            print(i, end='')
+            print('\n')
+            start_of_line = True
+        else:
+            print(i, end='')
+            start_of_line = False
