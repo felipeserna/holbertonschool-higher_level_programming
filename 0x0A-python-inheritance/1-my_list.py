96aad104259617a6e5a3430718ea616c9c3fab74
Felipe Serna <feserna86@gmail.com>
2020-06-02 20:32:50 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list"""
+
+
+class MyList(list):
+    """inherits from list"""
+
+
+    def print_sorted(self):
+        """
+        Prints the list sorted(ascending sort)
+
+        """
+
+        print(sorted(self))
