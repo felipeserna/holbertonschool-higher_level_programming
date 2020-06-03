a16bebfb7582c8c33d4efb2d961b1865fbeef3dc
Felipe Serna <feserna86@gmail.com>
2020-06-03 22:14:16 +0000

    hot fix

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -6,9 +6,8 @@ def number_of_lines(filename=""):
     """returns the number of lines of a text file
     Args: filename: string
     """
-    number_of_lines = 0
-
-    with open("my_file_0.txt", mode='r', encoding="utf-8") as my_file:
-        while (my_file.readline() != ''):
+    with open("my_file_0.txt", mode='r', encoding='utf-8') as my_file:
+        number_of_lines = 0
+        for line in my_file:
             number_of_lines += 1
-        return number_of_lines
+        return (number_of_lines)
