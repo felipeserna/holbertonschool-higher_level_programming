5532e428349e702aa85686b239953b678b13b40c
Felipe Serna <feserna86@gmail.com>
2020-07-14 00:47:52 +0000

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -17,18 +17,36 @@ VALUES
 	"John",
 	"10"
 );
+INSERT INTO second_table (
+       id,
+       name,
+       score
+)
+
 VALUES
 	(
 	"2",
 	"Alex",
 	"3"
 );
+INSERT INTO second_table (
+       id,
+       name,
+       score
+)
+
 VALUES
 	(
 	"3",
 	"Bob",
 	"14"
 );
+INSERT INTO second_table (
+       id,
+       name,
+       score
+)
+
 VALUES
 	(
 	"4",
