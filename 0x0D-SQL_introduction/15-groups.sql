bd15a99bd215427a0bcee50921dd6caa0e2e3a69
Felipe Serna <feserna86@gmail.com>
2020-07-14 03:34:03 +0000

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -5,7 +5,7 @@
 -- ** the number of records for this score with the label number
 -- * The list should be sorted by the number of records (descending)
 -- * The database name will be passed as an argument to the mysql command
-SELECT score FROM second_table
-COUNT(score) AS number
+SELECT score,
+COUNT(score) AS number FROM second_table
 GROUP BY score
-ORDER BY number DESC;
+ORDER BY score DESC;
