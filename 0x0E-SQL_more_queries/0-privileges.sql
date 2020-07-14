8afddfb5c98e1e2311b0db9fecf5d1b54375ce02
Felipe Serna <feserna86@gmail.com>
2020-07-14 17:35:40 +0000

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- on your server (in localhost).
-SHOW GRANTS user_0d_1,
-SHOW GRANTS user_0d_2;
+SHOW GRANTS for user_0d_1,
+SHOW GRANTS for user_0d_2;
