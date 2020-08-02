b28b6a85f09937315883853d5a4e04d92ae3af04
Felipe Serna <feserna86@gmail.com>
2020-08-02 19:54:04 +0000

    checking changes in 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,4 +5,8 @@
 -- tv_show_genres.genre_id
 -- * You can use only one SELECT statement
 -- * The database name will be passed as an argument of the mysql command
-SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
+SELECT t_s.title, t_s_g.genre_id
+FROM tv_shows AS t_s
+INNER JOIN tv_show_genres AS t_s_g
+ON t_s.id = t_s_g.show_id
+GROUP BY t_s.title ASC, t_s_g.genre_id ASC;
