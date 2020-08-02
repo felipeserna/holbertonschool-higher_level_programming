b969301afe831426dc32b9a676b7cee6ca795ded
Felipe Serna <feserna86@gmail.com>
2020-08-02 20:19:33 +0000

    checking changes in 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,9 @@
 -- tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` WHERE g.`genre_id` IS NULL ORDER BY s.`title`, g.`genre_id`;
+SELECT t_s.title, t_s_g.genre_id
+FROM tv_shows AS t_s
+LEFT JOIN tv_show_genres AS t_s_g
+ON t_s.id = t_s_g.show_id
+WHERE t_s_g.show_id IS NULL
+GROUP BY t_s.title ASC, t_s_g.genre_id ASC;
