3512310b22e0aead88ec7428ffbace1079258381
Felipe Serna <feserna86@gmail.com>
2020-08-02 17:26:33 +0000

    checking changes in 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,7 +5,7 @@
 -- * Results must be sorted in ascending order by cities.id
 -- * You are not allowed to use the JOIN keyword
 -- * The database name will be passed as an argument of the mysql command
-SELECT citiesid, name
+SELECT cities.id, cities.name
 FROM cities
 WHERE state_id = 1
 GROUP BY id;
