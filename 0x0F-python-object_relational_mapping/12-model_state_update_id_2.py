35f055e43b2be935dc3b3fd2b9213b3cc5ab6bed
Felipe Serna <feserna86@gmail.com>
2020-08-09 18:20:59 +0000

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module - changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    session.query(State). \
+        filter_by(id=2). \
+        update({State.name: "New Mexico"}, synchronize_session=False)
+    # confirm the change
+    session.commit()
+    session.close()
