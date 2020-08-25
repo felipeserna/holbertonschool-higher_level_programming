73949c84ca29d5cc5839b526834847bc364f5d19
Felipe Serna <feserna86@gmail.com>
2020-08-25 03:08:01 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that takes URL and a email, sends POST reques tto the URL"""
+"""
+Script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
 
 import requests
 from sys import argv
@@ -10,10 +13,10 @@ if __name__ == "__main__":
     else:
         q = argv[1]
 
-    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
 
     try:
-        data = res.json()
+        data = response.json()
         if data:
             print("[{}] {}".format(data["id"], data["name"]))
         else:
