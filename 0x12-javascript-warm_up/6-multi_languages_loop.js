adea220b96e09548a344c7b2174780e78d56eba3
Felipe Serna <feserna86@gmail.com>
2020-09-15 21:22:20 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+Prints 3 lines using an
+array of string and a loop
+*/
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; array[i]; i++) {
+  console.log(array[i]);
+}
