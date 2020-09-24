e7103838966e13732eb8080e04ca469c8ebd0576
Felipe Serna <feserna86@gmail.com>
2020-09-24 20:06:23 +0000

    task 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,10 @@
+/*
+Write a Javascript script that adds the class red to the HTML
+tag HEADER to red (#FF0000) when the user clicks on the tag DIV#red_header:
+
+- You can’t use document.querySelector to select the HTML tag
+- You must use the jQuery API
+*/
+$('DIV#red_header').click(function () {
+  $('header').addClass('red');
+});
