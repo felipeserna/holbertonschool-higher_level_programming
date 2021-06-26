69a6a4e35ac4c8a9badb2a76d16971f5294aa314
felipeserna <feserna86@gmail.com>
2021-06-26 18:56:12 -0500

    Correcting pep8 Adding comments for better understanding

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Adds two integers and returns the result
+"""
+
+
 def add(a, b):
+    """
+    Returns the value of a + b
+    """
     result = a + b
     return (result)
