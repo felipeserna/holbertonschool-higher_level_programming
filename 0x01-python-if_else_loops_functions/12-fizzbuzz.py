8ff62e879b8a2b5296796085be3afee8eb7bb84b
felipeserna <feserna86@gmail.com>
2021-06-26 19:24:07 -0500

    pep8 and 'while' instead of 'for' loop: - pep8 correction was done for correcting style. - 'for' loop was changed to 'while' loop for practicing my skills.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+"""
+Prints the numbers from 1 to 100 separated by a space.
+"""
+
+
 def fizzbuzz():
-    for i in range(1, 101):
+    """
+    Returns: nothing
+    """
+    i = 1
+    while i <= 100:
+        # For numbers which are multiples of both three and five
+        # print FizzBuzz.
         if (i % 3 == 0) and (i % 5 == 0):
             print("FizzBuzz ", end="")
+        # For multiples of three print Fizz
         elif (i % 3 == 0):
             print("Fizz ", end="")
+        # For multiples of five print Buzz
         elif (i % 5 == 0):
             print("Buzz ", end="")
         else:
             print("{:d} ".format(i), end="")
+        i += 1
