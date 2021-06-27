734696975c50ddcabc863391a31a2d1df63ab88f
felipeserna <feserna86@gmail.com>
2021-06-26 19:02:12 -0500

    Correcting pep8 style Adding comments for better understanding of function 'pow'

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Computes 'a' to the power of 'b' and return the value.
+"""
+
+
 def pow(a, b):
+    """
+    Returns the value of a ^ b
+    """
     power = a ** b
     return (power)
