06d8e6c7d97a6fc8ae2f857625bfbba2b0f32205
Felipe Serna <feserna86@gmail.com>
2021-07-28 15:11:23 -0500

    this task is wrong

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- creates a table called first_table in the current database
 -- in your MySQL server.
-CREATE TABLE first_table (
+ddCREATE TABLE first_table (
        id INT,
        name VARCHAR(256)
 );
